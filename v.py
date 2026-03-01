82fda1966275775a718c8860d21e4e5dbfed74e3
Toufik Medjber <toufikmedjber2@gmail.com>
2026-03-01 10:28:18 +0100

    1

---
+++ v.py
@@ -0,0 +1,47 @@
+import requests
+import time
+
+# === CONFIGURATION ===
+URL = "https://trouverunlogement.lescrous.fr/tools/42/search?bounds=5.9409699_47.3200746_6.0834844_47.2006872"
+BOT_TOKEN = "8758339041:AAH7cXum9-_XlaIvVBu1S4e9ydwNlMlNbOU"
+CHAT_ID = "5825590629"
+CHECK_INTERVAL = 30  # toutes les 3 minutes
+
+# === Fonction Telegram ===
+def envoyer_message_telegram(message):
+    try:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {"chat_id": CHAT_ID, "text": message}
+        requests.post(url, data=payload)
+    except Exception as e:
+        print("Erreur d'envoi Telegram :", e)
+
+# === Fonction principale ===
+def verifier_et_alerter():
+    deja_signale = False
+    print("✅ Bot CROUS lancé.")
+    envoyer_message_telegram("🚀 Le bot CROUS est lancé et surveille les logements...")
+
+    while True:
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            response = requests.get(URL, headers=headers)
+            contenu = response.text
+
+            if "Aucun logement trouv" not in contenu:
+                if not deja_signale:
+                    print("📢 Logement trouvé !")
+                    envoyer_message_telegram("🟢 Un logement CROUS est disponible ! 🔗 " + URL)
+                    deja_signale = True
+            else:
+                print("❌ Aucun logement. On continue...")
+                deja_signale = False
+
+        except Exception as e:
+            print("❗ Erreur :", e)
+            envoyer_message_telegram(f"❗ Le bot a eu un problème : {e}")
+
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == "__main__":
+    verifier_et_alerter()
